0f9e6a9e94f9f688c059e36d8e5cc360a6d49778
Fani Karina <140600965+Fanikarina@users.noreply.github.com>
2023-10-04 15:03:31 +0700

    Add files via upload

---
+++ arrow_funct.js
@@ -0,0 +1,10 @@
+const Login = (usn, pw) => {
+    console.log("--------------------")
+    console.log("       LOGIN        ")
+    console.log("--------------------")
+    console.log("Username = " + usn)
+    console.log("Password = " + pw)
+    console.log("--------------------")
+}
+
+Login("Fani karina", "1234")
