e392cfd43a5f5ee5bc3da422729a9357fc07c339
Fani Karina <140600965+Fanikarina@users.noreply.github.com>
2023-10-04 15:00:50 +0700

    Add files via upload

---
+++ funct.js
@@ -0,0 +1,10 @@
+function Login (usn, pw) {
+    console.log  ("---------------------")
+    console.log  ("        LOGIN        ")
+    console.log  ("---------------------")
+    console.log  ("Username = " + usn)
+    console.log  ("password = " + pw)
+    console.log  ("---------------------")
+}
+
+Login("Fani karina", "abcd")
