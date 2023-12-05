d81992345ae43753c15f877b1538d71e82d151c3
Fani Karina <140600965+Fanikarina@users.noreply.github.com>
2023-12-05 12:10:03 +0700

    Add files via upload

---
+++ pm-31-main/profile_arrow_function.js
@@ -0,0 +1,7 @@
+const login = (namaDepan, namaBelakang, email) => {
+    console.log("Login screen")
+    console.log("Halo " + namaDepan + namaBelakang)
+    console.log("Email kamu : " + email)
+}
+
+login("Fani", " Karina Agustina", "222310260@smkn4-tsm.sch.id")
---
+++ pm-31-main/profile_reg_function.js
@@ -0,0 +1,7 @@
+function home(namaDepan, namaBelakang, email) {
+    console.log("Selamat datang di home screen")
+    console.log("Halo " + namaDepan + namaBelakang)
+    console.log("Email kamu : " + email)   
+}
+
+home("Fani", " Karina Agustina", "222310260@smkn4-tsm.sch.id")
